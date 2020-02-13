7c4b3f4498b1b2b64fc4e5c3f14b23e20a9137d8
Tommi Linnamaa <linnamat@student.tut.fi>
2020-02-13 09:20:16 +0200

    added timer counting up

---
+++ front/src/timer/Timer.tsx
@@ -0,0 +1,51 @@
+import React, { useState, useEffect }from 'react';
+
+
+const Timer = (): JSX.Element => {
+
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  
+  const toggle = () => {
+    setIsActive(!isActive);
+  }
+
+  const reset = () => {
+    setSeconds(0);
+    setIsActive(false);
+  }
+
+  useEffect(
+    () => {
+      let timer: null | NodeJS.Timeout = null;
+    if(isActive) {
+      timer = setTimeout(() => {
+        console.log("are we here?")
+        setSeconds(s => s + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0 )
+    return () => {
+      if(timer) {
+        clearInterval(timer);
+      }
+    }
+    }, [seconds, isActive]);
+
+  return (
+    <div className="timer">
+      <div className="time">
+        {seconds}s
+      </div>
+      <div className="row">
+        <button className="button-primary" onClick = {toggle}>
+          {isActive ? 'Pause' : 'Start'}
+        </button>
+        <button className="button-secondary" onClick={reset}>
+          Reset
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Timer;
